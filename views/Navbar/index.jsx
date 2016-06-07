ab4cac5b9b7988fc8708cf0897c5e0ab5f6b0c12
joephon <306519779@qq.com>
2016-06-07 21:54:35 +0800

    added;

--- views/Navbar/index.jsx
+++ views/Navbar/index.jsx
@@ -10,12 +10,12 @@ module.exports = React.createClass({
     const activeStyle = {borderBottom: '3px solid white',transition: 'all 0.2s ease-in'}
     return <div className={this.props.hasShadow ? 'navbar navbar-light bg-faded hidden-sm-down topbar navbar-fixed-top row' : 'navbar navbar-light bg-faded hidden-sm-down topbar-shadow navbar-fixed-top row'}>
       <div className='container'>
-        <div className='col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 nav-logo'>
+        <div className='col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 nav-logo'>
           <div className='logo-img'>
             <IndexLink to="/"><img src='../../images/logo.png' className="navbar-img"></img></IndexLink>
           </div>
         </div>
-        <div className='col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10'>
+        <div className='col-xs-11 col-sm-11 col-md-11 col-lg-11 col-xl-11'>
           <ul className="nav nav-inline">
             <li className="nav-item">
               <Link to="/evaluation" className="nav-link navbar-text" activeStyle={activeStyle}>{locales.zh_CN.evaluation}</Link>
