6b4d1dc8af7201282e88d5eeb3cd7f0e1ccae075
nodejs <shinchou@shinchou.local>
2016-06-13 10:55:28 +0800

    about all

--- views/Home/Header/index.jsx
+++ views/Home/Header/index.jsx
@@ -11,8 +11,8 @@ module.exports=React.createClass({
           </div>
           <div className='col-sm-12 col-md-8 col-xs-12 col-lg-8'>
             <div className="right-text">
-              <h1 className='text-line-1'>这里有一套开发方案，快速估价</h1>
-              <h4 className='text-line-2'>2分钟，了解您的项目费用和周期</h4>
+              <h1 className='text-line-1'>这里有一套开发方案，帮你快速估价</h1>
+              <h4 className='text-line-2'>2分钟，了解你的项目费用和周期</h4>
               <Link to="/evaluation">
                 <button type="button" className="btn btn-primary header-btn">马上了解</button>
               </Link>
