f733a2269b59f9a00ad711b904344a34e9e6d167
nodejs <shinchou@shinchou.local>
2016-07-11 11:44:45 +0800

    reset

--- views/Contact/index.jsx
+++ views/Contact/index.jsx
@@ -6,18 +6,26 @@ module.exports = React.createClass({
     document.title = locales.zh_CN.contact
   },
   render(){
-    return <div className='contact'>
+    return <div className='contact-container'>
       <div className="container">
-        <p className='contact-intro'>
-          newTeo乃潮州市潮安区新潮网络科技有限公司（新潮科技）麾下核心技术&设计团队。
-          主要提供现代互联网应用的需求梳理、UI设计、程序开发和维护等服务。
-          扁平化管理，开放式思维是团队理念，
-          敢于运用前沿技术、新颖模式为团队风格，
-          自我革新，精益求精乃团队品质。
-          一群年轻人因为类似的想法而聚集到一起，为实现共同的愿景而不懈努力。
-          这便是newTeo，这正是新潮科技！
-        </p>
-    </div>
+        <div className='row'>
+          <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+            <img src='/images/联系界面.png' className='img'/>
+            <p className='contact-intro'>
+              newTeo乃潮州市潮安区新潮网络科技有限公司（新潮科技）麾下核心技术&设计团队。
+              主要提供现代互联网应用的需求梳理、UI设计、程序开发和维护等服务。
+              扁平化管理，开放式思维是团队理念，
+              敢于运用前沿技术、新颖模式为团队风格，
+              自我革新，精益求精乃团队品质。
+              一群年轻人因为类似的想法而聚集到一起，为实现共同的愿景而不懈努力。
+              这便是newTeo，这正是新潮科技！
+            </p>
+          </div>
+          <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 people'>
+          <img src='images/人物.png' className='people-img'/>
+          </div>
+        </div>
+      </div>
   </div>
   }
 })
