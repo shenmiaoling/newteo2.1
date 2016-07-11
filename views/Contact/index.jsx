0e0fa69016c08635230b2c1672c7b183c571cde5
nodejs <shinchou@shinchou.local>
2016-07-11 16:40:58 +0800

    reset

--- views/Contact/index.jsx
+++ views/Contact/index.jsx
@@ -9,9 +9,9 @@ module.exports = React.createClass({
     return <div className='contact-container'>
       <div className="container">
         <div className='row'>
-          <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+          <div className='col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 left-intro'>
             <img src='/images/联系界面.png' className='img'/>
-            <p className='contact-intro'>
+            <h5 className='contact-intro'>
               newTeo乃潮州市潮安区新潮网络科技有限公司（新潮科技）麾下核心技术&设计团队。
               主要提供现代互联网应用的需求梳理、UI设计、程序开发和维护等服务。
               扁平化管理，开放式思维是团队理念，
@@ -19,9 +19,10 @@ module.exports = React.createClass({
               自我革新，精益求精乃团队品质。
               一群年轻人因为类似的想法而聚集到一起，为实现共同的愿景而不懈努力。
               这便是newTeo，这正是新潮科技！
-            </p>
+            </h5>
+
           </div>
-          <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 people'>
+          <div className='col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3'>
           <img src='images/人物.png' className='people-img'/>
           </div>
         </div>
--- views/Product/index.jsx
+++ views/Product/index.jsx
@@ -11,19 +11,27 @@ module.exports = React.createClass({
         <div className='col-lg-6 col-xl-6 col-xs-12 col-sm-6 col-md-6 production-row'>
             <img src='/images/product_1.jpg' className='product-image'/>
             <div className='product-content'>
-              <p className='product-text'>主体内容包含但不限：</p>
-              <p className='product-text-content'>就是的卡上的和任务接客户回家撒复合弓还是给大家发货黄金咖啡几乎覆盖</p>
-              <p className='product-text'>主体内容包含但不限：</p>
-              <p className='product-text-content'>就是的卡上的和任务接客户回家撒复合弓还是给大家发货黄金咖啡几乎覆盖</p>
+              <p className='product-text'>JSX-China：</p>
+              <p className='product-text-content'>提倡React ＋ REST API 的Web开发模式，社区将逐渐形成一套关于React开发的知识库，将覆盖快速入门、实战模式、实战进阶、延伸探讨和同学们在开发过程中所遭遇的形形色色疑难杂症的汇总及解决方案。
+              如果你是一枚新手，社区的知识库将助你一路乘风破浪。
+如果你是一枚老鸟，快些儿加入到社区共建中来，大声说～你是怎么玩的。</p>
+              <p className='product-text'>敬请期待：</p>
+              <p className='product-text-content'>这将是一款别开生面的产品～简约，不简单；专注，不浮夸；随性，却用心；不俗，很好用！
+Beta版内测中。。。。。。
+              </p>
             </div>
         </div>
         <div className='col-lg-6 col-xl-6 col-xs-12 col-sm-6 col-md-6 production-row'>
             <img src='/images/product_1.jpg' className='product-image'/>
             <div className='product-content'>
-              <p className='product-text'>主体内容包含但不限：</p>
-              <p className='product-text-content'>就是的卡上的和任务接客户回家撒复合弓还是给大家发货黄金咖啡几乎覆盖</p>
-              <p className='product-text'>主体内容包含但不限：</p>
-              <p className='product-text-content'>就是的卡上的和任务接客户回家撒复合弓还是给大家发货黄金咖啡几乎覆盖</p>
+              <p className='product-text'>JSX-China：</p>
+              <p className='product-text-content'>提倡React ＋ REST API 的Web开发模式，社区将逐渐形成一套关于React开发的知识库，将覆盖快速入门、实战模式、实战进阶、延伸探讨和同学们在开发过程中所遭遇的形形色色疑难杂症的汇总及解决方案。
+              如果你是一枚新手，社区的知识库将助你一路乘风破浪。
+如果你是一枚老鸟，快些儿加入到社区共建中来，大声说～你是怎么玩的。</p>
+              <p className='product-text'>敬请期待：</p>
+              <p className='product-text-content'>这将是一款别开生面的产品～简约，不简单；专注，不浮夸；随性，却用心；不俗，很好用！
+Beta版内测中。。。。。。
+              </p>
             </div>
         </div>
       </div>
